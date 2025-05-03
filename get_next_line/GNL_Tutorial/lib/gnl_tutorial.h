24139ca1cf935f2d5b7501caffddea46d6028bc1
Thiago Henrique Freitas Do Nascimento <thenriqu@c1r13s1.42porto.com>
2025-05-03 12:43:44 +0100

    Added tester library v0.1

---
+++ get_next_line/GNL_Tutorial/lib/gnl_tutorial.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl_tutorial.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thenriqu <thenriqu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 12:21:26 by thenriqu          #+#    #+#             */
+/*   Updated: 2025/05/03 12:22:57 by thenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GNL_TUTORIAL_H
+# define GNL_TUTORIAL_H
+
+// Macros:
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// Includes:
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+// Prototypes:
+
+#endif
