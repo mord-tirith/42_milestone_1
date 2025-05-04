3c1704f770df832dcb4a5a2ea83742851af1ae2a
mord-tirith <thiagohfreitasn@gmail.com>
2025-05-04 23:59:41 +0100

    added comments to .c files of complete project

--- get_next_line/GNL_Tutorial/5_Get_Next_Line/files/complete_gnl.c
+++ get_next_line/GNL_Tutorial/5_Get_Next_Line/files/complete_gnl.c
@@ -1,34 +1,45 @@
 #include "gnl.h"
 
+
+/*
+ 	Essa função é responsável por devolver apenas o texto até a \n pro usuário, além de
+	limpar o buffer para deixá-lo pronto para a próxima vez que GNL for chamada
+ */
 static char *ft_handle_nl(char *buffer, char *new_line)
 {
     int nl_index;
 
-    nl_index = ft_locate_nl(new_line);
-    if (nl_index >= 0)
-        new_line[nl_index + 1] = '\0';
-    ft_memmove_nl(buffer);
-    return (new_line);
+	nl_index = ft_locate_nl(new_line); // Buscamos em que index de new_line se encontra o \n
+	if (nl_index >= 0)
+		new_line[nl_index + 1] = '\0'; // Botamos um \0 um char depois da \n, cortando a new_line
+	ft_memmove_nl(buffer); // Chamamos nossa helper que lida com o buffer
+	return (new_line);
 }
 
+/*
+ 	Nosso loop de leitura, que está aqui porquê a Norminette é uma bitch
+ */
 static char    *ft_read_loop(char *buffer, char *new_line, int fd)
 {
     ssize_t bytes_read;
 
-	ft_memmove_nl(buffer);
-    while (1)
+	ft_memmove_nl(buffer); // Para evitar lixo deixado no buffer em rodadas anteriores, limpamos ele antes de ler.
+						   // Isso não é problemático porquê nós nunca chegamos nessa função sem ter antes passado
+						   // o que estivesse no buffer para a new_line: podemos limpar ele sem medo!
+
+    while (1) // Todas condições pra terminar o loop dependem do que ocorre em bytes_read, portanto podemos fazer um loop "eterno"
     {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        bytes_read = read(fd, buffer, BUFFER_SIZE); // lemos do fd para nosso buffer
         if (bytes_read < 0)
-            return (ft_safe_free(new_line, buffer));
-        if (bytes_read == 0)
+            return (ft_safe_free(new_line, buffer)); // limpamos tudo e retornamos NULL se a leitura deu errado
+        if (bytes_read == 0) // saímos do loop se tentamos ler mas já estávamos no fim do fd
 			break ;
-		new_line = ft_join_free(new_line, buffer);
+		new_line = ft_join_free(new_line, buffer); // aqui a gente adiciona o que estiver no buffer à new_line
         if (!new_line)
             return (NULL);
-        if (ft_locate_nl(buffer) >= 0)
+        if (ft_locate_nl(buffer) >= 0) // caso uma \n tenha entrado no buffer nessa rodada, está na hora de parar de ler: temos uma linha inteira!
             break ;
-		ft_memmove_nl(buffer);
+		ft_memmove_nl(buffer); // para evitar lixo de memória no fim do arquivo, limpamos o buffer de novo
     }
 	return (new_line);
 }
@@ -38,14 +49,23 @@ char    *get_next_line(int fd)
     static char buffer[BUFFER_SIZE + 1];
     char        *new_line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
+    if (fd < 0 || BUFFER_SIZE <= 0) // saímos cedo se o usuário mandou um fd ou BUFFER_SIZE inválido
 		return (NULL);
-	new_line = NULL;
-    if (buffer[0] != '\0')
+
+	new_line = NULL; // para evitar erros causados por lixo de memória, inicializamos new_line como NULL
+
+    if (buffer[0] != '\0') // caso ainda haja texto salvo no buffer, adicionamos ele à new_line antes de começar a ler de novo
         new_line = ft_join_free(new_line, buffer);
-    if (ft_locate_nl(buffer) >= 0)
-        return (ft_handle_nl(buffer, new_line));
-    new_line = ft_read_loop(buffer, new_line, fd);
-    new_line = ft_handle_nl(buffer, new_line);
-    return (new_line);
+
+    if (ft_locate_nl(buffer) >= 0)                // se já houver uma \n dentro do buffer, isso quer dizer que já há uma linha inteira
+        return (ft_handle_nl(buffer, new_line));  // carregada e pronta pra ser devolvida ao usuário: podemos retornar já daqui, sem ler!
+
+    new_line = ft_read_loop(buffer, new_line, fd); // chamamos nosso loop de leitura, que vai correr até uma de 3 coisas ocorrer:
+												   // um erro de leitura, achar uma \n ou acabar o fd.
+	if (!new_line)
+		return (NULL);	// como read_loop faz mallocs, temos que nos proteger contra um erro de malloc
+
+    new_line = ft_handle_nl(buffer, new_line);     // tratamos do buffer e da new_line antes de devolver
+    
+	return (new_line);
 }
--- get_next_line/GNL_Tutorial/5_Get_Next_Line/files/utils.c
+++ get_next_line/GNL_Tutorial/5_Get_Next_Line/files/utils.c
@@ -1,5 +1,9 @@
 #include "gnl.h"
 
+/*
+ 	Sem comentários
+ */ 
+
 size_t	ft_strlen(char *str)
 {
 	size_t	i;
@@ -12,33 +16,52 @@ size_t	ft_strlen(char *str)
 	return (i);
 }
 
+/*
+ 	Nosso coração da GNL, essa função adiciona os chars que estiverem em buffer à new_line
+	Além disso, ela já dá free na new_line antiga, garantindo que nós temos manuseio perfeito
+	de memória sem se preocupar com malabarismo de ponteiros
+ */
 char	*ft_join_free(char *line, char *buffer)
 {
 	size_t	i;
 	size_t	j;
 	char	*big_line;
 
-	if (!line)
+	if (!line)				// buffer nunca pode não existir, mas como line pode ser NULL, temos que garantir que ela existe
 	{
 		line = malloc(1);
 		if (!line)
 			return (NULL);
 		line[0] = '\0';
 	}
-	big_line = malloc(sizeof(char) * (ft_strlen(line) + ft_strlen(buffer) + 1));
+
+	big_line = malloc(sizeof(char) * (ft_strlen(line) + ft_strlen(buffer) + 1)); // criamos a nova linha com espaço o bastante pra 
+																				 // guardar a new_line antiga e o que houver no buffer
+	
 	if (big_line)
 	{
-		i = -1;
-		j = -1;
-		while (line[++i])
+		i = -1; // Criamos esses iteradores com -1 pra economizar 3 linhas em cada loop
+		j = -1; // jogando o "++" dentro da condição do loop
+		
+		while (line[++i])					// primeiro copiamos da linha antiga para a nova...
 			big_line[i] = line[i];
-		while (buffer[++j])
+		
+		while (buffer[++j])					// depois copiamos do buffer para a linha nova
 			big_line[i + j] = buffer[j];
-		big_line[i + j] = '\0';
+		
+		big_line[i + j] = '\0';				// e metemos \0 no fim ^^
 	}
-	free(line);
+	free(line);	// como prometido no título, damos free na linha original.
+				// amizade encerrada com line: agora big_line é minha melhor amiga
+
 	return (big_line);
 }
+
+/*
+ 	Função vital pra nossa GNL, ela retorna o index onde se encontra a primeira \n da string que recebe.
+	Além disso, ela também retorna -1: algo essencial para podermos usar essa função tanto como uma
+	verificação de "onde está a \n" quando de "há uma \n nessa string?"
+ */
 
 ssize_t	ft_locate_nl(char *str)
 {
@@ -56,35 +79,49 @@ ssize_t	ft_locate_nl(char *str)
 	return (-1);
 }
 
+/*
+ 	Nossa controladora do buffer: essa função copia o que está depois da primeira \n no buffer
+	pro começo dele, depois limpa tudo que resta pra nunca termos lixo em nosso buffer.
+	Além disso, a função é esperta o bastante para limpar o buffer inteiro quando não há
+	nenhuma \n dentro dele!
+*/
 void	ft_memmove_nl(char *buffer)
 {
-	ssize_t	i;
-	ssize_t	nli;
+	ssize_t	i;		// Iterador simples
+	ssize_t	nli;	// Iterador para a primeira \n no buffer
 
 	i = 0;
-	nli = ft_locate_nl(buffer) + 1;
-	while (buffer[i + nli] && nli)
+	nli = ft_locate_nl(buffer) + 1; // nós precisamos limpar até a \n, ou seja, precisamos copiar de
+									// 1 char a mais que o index de \n
+	
+	while (buffer[i + nli] && nli) // rodamos esse loop enquanto o iterador que começa na \n do buffer não chegar ao \0
 	{
-		buffer[i] = buffer[i + nli];
+		buffer[i] = buffer[i + nli]; // copiamos à partir da \n pro começo do buffer
 		i++;
 	}
-	while (i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE) // agora é hora de limpar o que houver sobrado!
 	{
-		buffer[i] = '\0';
-		i++;
+		buffer[i] = '\0';   // um detalhe importante é que se ft_locate_nl(buffer) retornou -1, o valor de i nunca mudou de 0
+		i++;				// ou seja, se não havia \n no buffer, vamos limpar ele inteiro
 	}
 }
+
+/*
+ 	Talvez desnecessária, essa função só serve para dar uma limpeza e free perfeitos
+	em buffer e new_line quando acontece um erro de leitura
+ */
 
 char	*ft_safe_free(char *line, char *buffer)
 {
 	size_t	i;
 
-	if (line)
+	if (line)		// um free seguro simples: se line não é NULL, damos free
 		free(line);
 	i = 0;
-	while (i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE) // e aqui uma limpeza garantida. buffer tem tamanho BUFFER_SIZE + 1
 	{
-		buffer[i] = '\0';
+		buffer[i] = '\0';	// ou seja, se limparmos ele do index 0 ao index BUFFER_SIZE, vamos 
+							// ter apagado tudo que havia nele
 		i++;
 	}
 	return (NULL);
